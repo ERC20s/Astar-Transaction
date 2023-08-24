41877036553adbda63d5494381b80d0051b44c7e
ERC20s <55540259+ERC20s@users.noreply.github.com>
2023-08-24 13:57:19 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -1,96 +1,65 @@
-import {
-  engine,
-  GltfContainer,
-  InputAction,
-  inputSystem,
-  Material,
-  MeshCollider,
-  pointerEventsSystem,
-  Transform
-} from '@dcl/sdk/ecs'
-import { Color4, Vector3 } from '@dcl/sdk/math'
-import { RequestManager } from 'eth-connect'
-import { createEthereumProvider } from '@dcl/sdk/ethereum-provider'
-import { getUserData } from '~system/UserIdentity'
-
-import { bounceScalingSystem, circularSystem } from './systems'
+import { engine, GltfContainer, Transform, pointerEventsSystem, InputAction, UiText} from '@dcl/sdk/ecs'
+import { Quaternion } from '@dcl/sdk/math'
 
 import { setupUi } from './ui'
-import { BounceScaling, Spinner } from './components'
-import { createCube } from './factory'
-// Defining behavior. See `src/systems.ts` file.
-engine.addSystem(circularSystem)
-engine.addSystem(bounceScalingSystem)
 
-const provider = createEthereumProvider()
-const requestManager = new RequestManager(provider)
 
 export function main() {
-  // draw UI
-  setupUi()
 
-  // fetch cube from Inspector
-  const cube = engine.getEntityOrNullByName('Magic Cube')
-  if (cube) {
-    // Give the cube a color
-    Material.setPbrMaterial(cube, { albedoColor: Color4.Blue() })
+    let items: { [key: string]: any } = {};
 
-    // Make the cube spin, with the circularSystem
-    Spinner.create(cube, { speed: 10 })
-
-    // Give the cube a collider, to make it clickable
-    MeshCollider.setBox(cube)
-
-    // Add a click behavior to the cube, spawning new cubes in random places, and adding a bouncy effect for feedback
-    pointerEventsSystem.onPointerDown(
-      { entity: cube, opts: { button: InputAction.IA_POINTER, hoverText: 'spawn' } },
-      () => {
-        createCube(1 + Math.random() * 8, Math.random() * 8, 1 + Math.random() * 8, false)
-        BounceScaling.createOrReplace(cube)
-      }
-    )
-  }
-  let avocado = engine.addEntity()
-
-  GltfContainer.create(avocado, {
-    src: 'models/avocado.glb'
-  })
-
-  Transform.create(avocado, {
-    position: Vector3.create(3, 1, 3)
-  })
-
-  pointerEventsSystem.onPointerDown(
-    {
-      entity: avocado,
-      opts: { button: InputAction.IA_POINTER, hoverText: 'Collect' }
-    },
-    function () {
-      console.log('CLICKED AVOCADO')
-      requestManager.eth_blockNumber().then((blockNumber) => {
-        console.log({ blockNumber })
-      })
-      let fromAddress
-      getUserData({}).then((userData) => {
-        if (userData.data?.hasConnectedWeb3) {
-          fromAddress = userData.data?.publicKey
-
-          console.log('fromAddress: ', fromAddress)
-          const toAddress = '0x6693ee891404284AA895340e5f68cade57533B47'
-          if (fromAddress) {
-            requestManager
-              .eth_sendTransaction({
-                to: toAddress,
-                from: fromAddress,
-                value: '0x2386F26FC10000', // 0.001 Astr
-                data: '0x'
-              })
-              .then((res) => {
-                console.log(res)
-              })
-          }
-        }
-      })
+    // Custom hover texts for each item
+    const hoverTexts: string[] = [
+        "Acala",
+        "Moonbeam",
+        "Astar",
+        // ... add texts for all 30 items
+    ];
+    
+    // Custom functions for each item's pointer down event
+    const pointerDownFunctions: Array<() => void> = [
+        () => { console.log("Action for Item 1"); },
+        () => { console.log("Action for Item 2"); },
+        () => { console.log("Action for Item 3"); },
+        // ... add functions for all 30 items
+    ];
+    
+    for (let i = 1; i <= 30; i++) {
+        console.log(`Creating ITEM ${i}`);
+      
+        // Store each entity in the items object with a dynamic key
+        items[`item${i}`] = engine.addEntity();
+      
+        GltfContainer.create(items[`item${i}`], {
+            src: `models/item${i}.gltf`,
+        });
+    
+        pointerEventsSystem.onPointerDown({
+                entity: items[`item${i}`],
+                opts: { 
+                    button: InputAction.IA_POINTER, 
+                    hoverText: hoverTexts[i - 1],  // Retrieve the custom hoverText using the current index
+                    maxDistance:100,
+                    showFeedback: true 
+                },
+            },
+            pointerDownFunctions[i - 1]  // Retrieve the custom function using the current index
+        );
+    
+        // Calculate x and y positions for a 6x5 grid with doubled spacing
+        let x = ((i - 1) % 6) * 2.5;   // Modulo operation gives column position, then multiply by 2 for spacing
+        let y = (Math.floor((i - 1) / 6)) * 3;  // Division gives row position, then multiply by 2 for spacing
+    
+        // Using the correct entity reference for Transform.create
+        Transform.create(items[`item${i}`], {
+            position: { x: x + 2, y: y+1, z: 10 }  // Place items on the x-y plane
+        });
     }
-  )
-}
+    
+    }
+    
+
+
+
+
+
